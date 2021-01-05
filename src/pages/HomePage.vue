382a54634f3333ddcb6e6eb28be17ace0608e5a0
ruzlicali16 <ruzlicali24@gmail.com>
2021-01-05 12:22:12 -0800

    initial commit on UI in home page

--- src/pages/HomePage.vue
+++ src/pages/HomePage.vue
@@ -1,48 +1,98 @@
 <template>
   <base-layout>
     <div class="wrapper ion-padding">
-      <ion-text color="success">
-        <h1 class="sub-header ion-no-margin ion-padding">
+      <ion-text color="medium">
+        <h1 class="sub-header ion-no-margin ion-padding-horizontal">
           Meranaw Learnign App
         </h1>
       </ion-text>
-      <div class="sub-content ion-padding">
-        <div>Here</div>
+      <div class="sub-content ion-padding-top">
+        <div>
+          <ion-card color="danger" button="true">
+            <ion-grid>
+              <ion-row>
+                <ion-col size="8">
+                  <ion-card-header
+                    class="card-header ion-no-padding ion-padding-start"
+                  >
+                    <ion-card-title>Learn</ion-card-title>
+                    <ion-card-subtitle>Card Subtitle</ion-card-subtitle>
+                  </ion-card-header>
+                  <img src="../../public/assets/design/study.png" width="160" />
+                </ion-col>
+                <ion-col class="second-column">
+                  <ion-button shape="round">
+                    <ion-icon slot="icon-only" :icon="arrowForwardOutline"></ion-icon>
+                  </ion-button>
+                </ion-col>
+              </ion-row>
+            </ion-grid>
+          </ion-card>
+
+          <ion-card color="orange" button="true">
+            <ion-grid>
+              <ion-row>
+                <ion-col size="8">
+                  <ion-card-header
+                    class="card-header ion-no-padding ion-padding-start"
+                  >
+                    <ion-card-title>Learn</ion-card-title>
+                    <ion-card-subtitle>Card Subtitle</ion-card-subtitle>
+                  </ion-card-header>
+                  <img src="../../public/assets/design/game.png" width="160" />
+                </ion-col>
+                <ion-col class="second-column">
+                  <ion-button shape="round">
+                    <ion-icon slot="icon-only" :icon="arrowForwardOutline"></ion-icon>
+                  </ion-button>
+                </ion-col>
+              </ion-row>
+            </ion-grid>
+          </ion-card>
+        </div>
       </div>
     </div>
   </base-layout>
 </template>
 
 <script>
-import { IonText } from "@ionic/vue";
+import {
+  IonText,
+  IonCard,
+  IonCardHeader,
+  IonCardTitle,
+  IonCardSubtitle,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonButton,
+  IonIcon,
+} from "@ionic/vue";
+import { arrowForwardOutline } from "ionicons/icons";
 
 export default {
   name: "HomePage",
   components: {
     IonText,
+    IonCard,
+    IonCardHeader,
+    IonCardTitle,
+    IonCardSubtitle,
+    IonGrid,
+    IonRow,
+    IonCol,
+    IonButton,
+    IonIcon,
+  },
+  data() {
+    return {
+      // icon
+      arrowForwardOutline,
+    };
   },
 };
 </script>
 
 <style scoped>
-@import url("https://fonts.googleapis.com/css2?family=Exo:wght@400;500&display=swap");
-.wrapper {
-  display: flex;
-  flex-flow: column;
-  height: 100%;
-}
-.sub-header {
-  height: 100px;
-  font-size: 3em;
-  display: flex;
-  align-items: center;
-  font-family: "Exo", sans-serif;
-  font-weight: bold;
-}
-.sub-content {
-  flex-grow: 1;
-  display: flex;
-  align-items: center;
-  justify-content: center;
-}
+@import '../styling/HomePage.css';
 </style>
