225756074738bc95c18d198571177c220110e3b4
ruzlicali16 <ruzlicali24@gmail.com>
2021-03-29 10:06:44 +0800

    initial design

--- src/pages/score/ScorePageContent.vue
+++ src/pages/score/ScorePageContent.vue
@@ -19,12 +19,26 @@
 
       <div class="sub-content">
         <ion-grid>
-          <ion-row class="ion-text-center">
-            <ion-col size="12">1st. Maryam</ion-col>
-            <ion-col size="12">2nd. Danieza</ion-col>
-            <ion-col size="12">3rd. Sarip</ion-col>
-            <ion-col size="12">4th. Amenah</ion-col>
-            <ion-col size="12">5th. Solaiman</ion-col>
+          <ion-row class="ion-justify-content-center ion-align-items-center">
+            <template v-for="(player, index) in topFivePlayer" :key="index">
+              <ion-col size="2">{{ player.place }}</ion-col>
+              <ion-col class="ion-no-padding" size="10">
+                <ion-card>
+                  <ion-card-content>
+                    <ion-row
+                      class="ion-justify-content-between ion-align-items-center"
+                    >
+                      <ion-col class="player-name" size="9">{{
+                        player.name
+                      }}</ion-col>
+                      <ion-col class="player-score ion-text-end" size="3">{{
+                        player.score
+                      }}</ion-col>
+                    </ion-row>
+                  </ion-card-content>
+                </ion-card>
+              </ion-col>
+            </template>
           </ion-row>
         </ion-grid>
       </div>
@@ -33,7 +47,14 @@
 </template>
 
 <script>
-import { IonText, IonGrid, IonRow, IonCol } from "@ionic/vue";
+import {
+  IonText,
+  IonGrid,
+  IonRow,
+  IonCol,
+  IonCard,
+  IonCardContent,
+} from "@ionic/vue";
 import { gameControllerOutline } from "ionicons/icons";
 import { Plugins } from "@capacitor/core";
 
@@ -46,9 +67,43 @@ export default {
     IonGrid,
     IonRow,
     IonCol,
+    IonCard,
+    IonCardContent,
   },
   data() {
     return {
+      topFivePlayer: [
+        {
+          name: "Maryam Daniez",
+          score: "1000",
+          time: "7 Seconds",
+          place: "1st",
+        },
+        {
+          name: "Amenah Solaiman",
+          score: "800",
+          time: "9 seconds",
+          place: "2nd",
+        },
+        {
+          name: "Amenah Solaiman",
+          score: "800",
+          time: "9 seconds",
+          place: "2nd",
+        },
+        {
+          name: "Amenah Solaiman",
+          score: "800",
+          time: "9 seconds",
+          place: "2nd",
+        },
+        {
+          name: "Amenah Solaiman",
+          score: "800",
+          time: "9 seconds",
+          place: "2nd",
+        },
+      ],
       // icons
       gameControllerOutline,
       statusBarColor: "#f3128a",
@@ -73,6 +128,17 @@ export default {
 ion-grid {
   --ion-grid-padding: 10px;
   --ion-grid-width-xs: 100px;
+}
+
+.player-name {
+  font-size: 15px;
+  font-family: "Exo", sans-serif;
+}
+
+.player-score {
+  font-size: 15px;
+  font-family: "Exo", sans-serif;
+  color: rgb(10, 170, 10);
 }
 
 .wrapper {
@@ -100,8 +166,8 @@ ion-grid {
   display: flex;
   align-items: center;
   justify-content: center;
-  margin: 0px 50px 0px 50px;
-  font-size: 2em;
+  margin: 0px 10px 0px 10px;
+  font-size: 20px;
   font-family: "Exo", sans-serif;
   font-weight: bold;
 }
