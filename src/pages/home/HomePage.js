eb681155843c967a847dfbe2817b16079a99f003
ruzlicali16 <ruzlicali24@gmail.com>
2021-04-18 11:30:18 +0800

    update home page

--- src/pages/home/HomePage.js
+++ src/pages/home/HomePage.js
@@ -9,24 +9,11 @@ import {
   IonCol,
   IonButton,
   IonIcon,
-  alertController,
-  loadingController,
-  toastController,
 } from "@ionic/vue";
 import { arrowForwardOutline } from "ionicons/icons";
 import { Plugins } from "@capacitor/core";
-import {
-  animalsQuery,
-  // colorsQuery,
-  // numbersQuery,
-  // wordsQuery,
-  firebaseDB,
-} from "../../firestore/firebaseInit.js";
-import Localbase from "localbase";
+import downloadContent from "../../services/download-content.service.js";
 import SliderScreen from "../../components/slider-screen/SliderScreen.vue";
-
-let localDB = new Localbase("db");
-localDB.config.debug = false;
 
 const { StatusBar } = Plugins;
 
@@ -90,83 +77,18 @@ export default {
       return statusBar;
     },
 
-    async presentDownloadConfirm() {
-      const alert = await alertController.create({
-        cssClass: "alert-custom-class",
-        header: "DOWNLOAD CONTENT",
-        message:
-          "Dear User, Please download the content for best User Experience and Offline Usage of the app. Thank You.",
-        buttons: [
-          {
-            text: "Download Here",
-            handler: () => {
-              console.log("Downloading...");
-              this.presentLoading();
-            },
-          },
-        ],
-      });
-      return alert.present();
-    },
-
-    async presentLoading() {
-      const loading = await loadingController.create({
-        message: "Downloading please wait...",
-      });
-      await loading.present();
-      this.downloadContent(loading);
-    },
-
-    async successToast() {
-      const toast = await toastController.create({
-        message: "Contents Successfully Downloaded.",
-        color: "success",
-        duration: 2000,
-      });
-      return toast.present();
-    },
-
     async hideSlider(value) {
       this.showSlider = await value;
     },
 
     /** BUSINESS LOGIC **/
     async checkContent() {
-      let db = await localDB.collection("contents").get();
-      if (db.length == 0) {
-        this.showSlider = true;
-      } else {
-        console.log("HAVE DB!");
-        let connectedRef = firebaseDB.ref(".info/connected");
-        connectedRef.on("value", (snap) => {
-          if (!snap.val()) {
-            // If Online
-            this.internetStatus = true;
-            console.log("connection: ", snap.val());
-            this.checkNewContentOnline();
-          }
-        });
+      const hasDB = await downloadContent.checkLocalContent();
+      if (hasDB) {
         this.showSlider = false;
+      } else {
+        this.showSlider = true;
       }
-    },
-    checkNewContentOnline() {
-      let checkDatabase = async (dbName) => {
-        const db = await localDB
-          .collection("contents")
-          .doc(dbName)
-          .get();
-        return db;
-      };
-
-      animalsQuery.onSnapshot((snapshot) => {
-        checkDatabase("animals").then((doc) => {
-          if (snapshot.docs.length != doc["animals"].length) {
-            console.log("NEW DATA ARRIVED");
-          } else {
-            console.log("NO NEW DATA!");
-          }
-        });
-      });
     },
   },
 };
